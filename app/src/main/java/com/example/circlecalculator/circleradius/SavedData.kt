8756577026dfcbc81f29779d0b768f5405b75566
C-Tin Nistor <kobo.turc@gmail.com>
2018-05-10 10:22:33 +0300

    in save file

--- app/src/main/java/com/example/circlecalculator/circleradius/SavedData.kt
+++ app/src/main/java/com/example/circlecalculator/circleradius/SavedData.kt
@@ -8,23 +8,23 @@ import android.widget.TextView
  * Created by Costi on 5/8/2018.
  */
 var savedResult:String = ""
-var file:String = "circleSave.txt"
+val file:String = "circleSave.txt"
 
 fun LoadSavedData(context: Context):String{
 
     try{
-        var fis = context.openFileInput(file)
-        var ase = fis.readBytes()
-        fis.close()
-        return String(ase)
+        val fi = context.openFileInput(file)
+        val rb = fi.readBytes()
+        fi.close()
+        return String(rb)
     }catch(e: Exception){
         return ""
     }
 }
 
 fun SaveDatafile(context:Context, resultSave: TextView){
-    var fos = context.openFileOutput(file, Context.MODE_PRIVATE)
-    fos.write(savedResult.toByteArray())
-    fos.close()
+    val fo = context.openFileOutput(file, Context.MODE_PRIVATE)
+    fo.write(savedResult.toByteArray())
+    fo.close()
     resultSave.text = savedResult
 }
